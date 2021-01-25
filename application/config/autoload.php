3ad735913d40682130b20176eeda180282a0e8d6
kirubha10 <kirubha106123@gmail.com>
2021-01-25 22:03:43 +0530

    url-helper

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array();
+$autoload['helper'] = array('url', 'form', 'cookie', 'file', 'security', 'string', 'html', 'text', 'inflector');
 
 /*
 | -------------------------------------------------------------------
